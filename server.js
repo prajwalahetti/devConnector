f26518a582ab663bb416a83da3281d68e1e2a341
prajwalahetti <parjwal.ahetti@gmail.com>
2022-03-04 20:38:35 +0530

    users module

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/',(req,res)=>{
     res.send('API running');
 });
 
+// define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profiles',require('./routes/api/profiles'));
 
 const PORT=process.env.PORT || 5000;
 
