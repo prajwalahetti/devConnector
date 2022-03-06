9e57d2a975225344e817c1c60879edc54f4a1329
prajwalahetti <parjwal.ahetti@gmail.com>
2022-03-07 01:44:36 +0530

    user and login on client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,50 @@ import './App.css';
 import React,{Fragment} from 'react'
 import Landing from './components/layout/Landing';
 import NavBar from './components/layout/NavBar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter,Routes, Route, Router } from 'react-router-dom';
+
+
 const App = () => {
   return (
     <Fragment>
-      <NavBar></NavBar>
-      <Landing/>
+    <BrowserRouter>
+
+      
+      <NavBar/>
+     
+      <Routes>
+     <Route  path='/' element={<Landing/> }/>
+     
+     
+     <Route  path='register' element={<Register/>} />
+      <Route   path='login' element={<Login/>} />
+     
+     </Routes>
+   
+    
+      
+      
+      </BrowserRouter>
       </Fragment>
+    
+    
   )
 }
 
 export default App
+
+/* <BrowserRouter>
+      <Fragment>
+      <NavBar/>
+      <Route  path='/' element={<Landing/> }/>
+      <section className='container'>
+      <Routes>
+            <Route  path='/register' element={<Register/>} />
+            <Route   path='/login' element={<Login/>} />
+        </Routes>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+     */
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from "react";
+import "../../App.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+const Login = () => {
+  const [formData, setFormData] = useState({
+    
+    email: "",
+    password: ""
+  });
+  const {  email,password} = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = async e => {
+    e.preventDefault();
+   
+        console.log('SUCCESS');
+  };
+  return (
+    <div className="container">
+      <Fragment>
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i>  Sign In to Your Account
+        </p>
+        <form className="form" onSubmit={(e) => onSubmit(e)}>
+         
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+            />
+            
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              autoComplete="on"
+              value={password}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+         
+          <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Dont have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </Fragment>
+    </div>
+  );
+};
+
+export default Login;
