51361e066239715ec7ff3d02cf79d61c26547df2
prajwalahetti <parjwal.ahetti@gmail.com>
2022-03-16 15:45:25 +0530

    prepared 2

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path=require('path');
+
 
 connectDB();
 
 // init middleware
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => {
-  res.send("API running");
-});
+
 
 // define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/post", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profiles"));
+
+// serve static assests in production
+
+if(process.env.NODE_ENV === 'production'){
+  // set static folder
+
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','index.html'))
+  })
+
+}
 
 const PORT = process.env.PORT || 5000;
 
